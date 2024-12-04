c526307880b20ac9dda229b0740a35bd20eec8c2
Daniel Hiller <dhiller@redhat.com>
2024-12-04 15:41:00 +0100

    contributions: add OWNERS file support
    Signed-off-by: Daniel Hiller <dhiller@redhat.com>

--- generators/cmd/contributions/main.go
+++ generators/cmd/contributions/main.go
@@ -23,9 +23,12 @@ import (
 	"flag"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
 	"kubevirt.io/community/pkg/contributions"
 	"kubevirt.io/community/pkg/orgs"
+	"kubevirt.io/community/pkg/owners"
 	"os"
+	"sort"
 )
 
 type ContributionReportOptions struct {
@@ -35,13 +38,15 @@ type ContributionReportOptions struct {
 	githubTokenPath    string
 	months             int
 	orgsConfigFilePath string
+	ownersFilePath     string
+	reportAll          bool
 }
 
 func (o ContributionReportOptions) validate() error {
 	if o.username != "" {
 		log.Infof("creating report for user %q", o.username)
-	} else if o.orgsConfigFilePath == "" {
-		return fmt.Errorf("username or orgs-config-file-path is required")
+	} else if o.orgsConfigFilePath == "" && o.ownersFilePath == "" {
+		return fmt.Errorf("username or orgs-config-file-path or owners-file-path is required")
 	}
 	if o.githubTokenPath == "" {
 		return fmt.Errorf("github token path is required")
@@ -67,6 +72,8 @@ func gatherContributionReportOptions() (ContributionReportOptions, error) {
 	fs.IntVar(&o.months, "months", 6, "months to look back for fetching data")
 	fs.StringVar(&o.githubTokenPath, "github-token", "/etc/github/oauth", "path to github token to use")
 	fs.StringVar(&o.orgsConfigFilePath, "orgs-file-path", "../project-infra/github/ci/prow-deploy/kustom/base/configs/current/orgs/orgs.yaml", "file path to the orgs.yaml file to check")
+	fs.StringVar(&o.ownersFilePath, "owners-file-path", "", "file path to the OWNERS file to check")
+	fs.BoolVar(&o.reportAll, "report-all", false, "whether to only report inactive users or all users")
 	err := fs.Parse(os.Args[1:])
 	return o, err
 }
@@ -92,13 +99,28 @@ func main() {
 
 	var reporter Reporter = DefaultReporter{}
 	userNames := []string{contributionReportOptions.username}
-	if contributionReportOptions.username == "" && contributionReportOptions.orgsConfigFilePath != "" {
-		orgsYAML, err := orgs.ReadFile(contributionReportOptions.orgsConfigFilePath)
-		if err != nil {
-			log.Fatalf("invalid arguments: %v", err)
+	if contributionReportOptions.username == "" {
+		if contributionReportOptions.ownersFilePath != "" {
+			ownersYAML, err := owners.ReadFile(contributionReportOptions.ownersFilePath)
+			if err != nil {
+				log.Fatalf("invalid arguments: %v", err)
+			}
+			userNames = ownersYAML.Reviewers
+			userNames = append(userNames, ownersYAML.Approvers...)
+			sort.Strings(userNames)
+			if !contributionReportOptions.reportAll {
+				reporter = InactiveOnlyReporter{}
+			}
+		} else if contributionReportOptions.orgsConfigFilePath != "" {
+			orgsYAML, err := orgs.ReadFile(contributionReportOptions.orgsConfigFilePath)
+			if err != nil {
+				log.Fatalf("invalid arguments: %v", err)
+			}
+			userNames = orgsYAML.Orgs[contributionReportOptions.org].Members
+			if !contributionReportOptions.reportAll {
+				reporter = InactiveOnlyReporter{}
+			}
 		}
-		userNames = orgsYAML.Orgs[contributionReportOptions.org].Members
-		reporter = InactiveReporter{}
 	}
 
 	for _, userName := range userNames {
@@ -111,10 +133,12 @@ func main() {
 			log.Fatalf("failed to report: %v", err)
 		}
 	}
+	fmt.Printf(reporter.Summary())
 }
 
 type Reporter interface {
 	Report(r contributions.ActivityReport, userName string) error
+	Summary() string
 }
 
 type DefaultReporter struct{}
@@ -128,14 +152,20 @@ func (d DefaultReporter) Report(r contributions.ActivityReport, userName string)
 	return nil
 }
 
-type InactiveReporter struct {
+func (d DefaultReporter) Summary() string {
+	return ""
+}
+
+type InactiveOnlyReporter struct {
 	inactiveUsers []string
 }
 
-func (d InactiveReporter) Report(r contributions.ActivityReport, userName string) error {
+func (d InactiveOnlyReporter) Report(r contributions.ActivityReport, userName string) error {
 	if r.HasActivity() {
+		log.Debugf("active user: %s", userName)
 		return nil
 	}
+	log.Infof("inactive user: %s", userName)
 	fmt.Printf(r.GenerateActivityLog())
 	_, err := r.WriteToFile("/tmp", userName)
 	if err != nil {
@@ -143,4 +173,13 @@ func (d InactiveReporter) Report(r contributions.ActivityReport, userName string
 	}
 	d.inactiveUsers = append(d.inactiveUsers, userName)
 	return nil
+}
+
+func (d InactiveOnlyReporter) Summary() string {
+	out, err := yaml.Marshal(d.inactiveUsers)
+	if err != nil {
+		log.Fatalf("failed to serialize: %v", err)
+	}
+	return fmt.Sprintf(`inactive users:
+%s`, string(out))
 }
---
+++ pkg/owners/types.go
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright the KubeVirt Authors.
+ *
+ */
+
+package owners
+
+type Owners struct {
+	Reviewers         []string `yaml:"reviewers"`
+	Approvers         []string `yaml:"approvers"`
+	EmeritusApprovers []string `yaml:"emeritus_approvers"`
+}
